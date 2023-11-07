7f97b2fc7df687effaffab24d0cf78010897961c
bafgreat <75862753+bafgreat@users.noreply.github.com>
2023-11-07 11:29:27 +0100

    modified appending into json

--- mofstructure/filetyper.py
+++ mofstructure/filetyper.py
@@ -75,22 +75,6 @@ def json_to_ase_atom(data,  encoder, filename):
     return
 
 
-def append_json_atom(data,  encoder, filename):
-    '''
-    append a data containing an ase atom object
-    '''
-    with open(filename, 'r+', encoding='utf-8') as f_obj:
-        # First we load existing data into a dict.
-        file_data = json.load(f_obj)
-        # Join new_data with file_data inside emp_details
-        file_data.update(data)
-        # Sets file's current position at offset.
-        f_obj.seek(0)
-        # convert back to json.
-
-        json.dump(data, f_obj, indent=4, sort_keys=False, cls=encoder)
-
-
 def append_json(new_data, filename):
     '''
     append a new data in an existing json file
